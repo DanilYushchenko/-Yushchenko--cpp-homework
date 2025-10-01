572bf37d30d80c9a78a9bc0c34213528881534c7
DanilYushchenko <Danil.Iushchenko@urfu.me>
2025-10-01 17:51:01 +0500

    Add files via upload

---
+++ homework22.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int x, y;
+    cout << "Введите первое число:";
+    cin >> x;
+    cout << "Введите второе число:";
+    cin >> y;
+ 
+    cout << "Сумма: " << x + y << endl;
+    cout << "Разность: " << x - y << endl;
+    cout << "Произведение: " << x * y << endl;
+ 
+    return 0;
+}
